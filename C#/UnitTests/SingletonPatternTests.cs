93efe5c91f4857fdbf1dfbdf2c0179ebff47725a
Joni Laukka <jonilaukka@hotmail.com>
2024-03-19 16:45:22 +0200

    C# Singleton Pattern

---
+++ C#/UnitTests/SingletonPatternTests.cs
@@ -0,0 +1,13 @@
+﻿using SingletonPattern;
+
+namespace UnitTests;
+[TestClass]
+public class SingletonPatternTests
+{
+  [TestMethod]
+  public void SingletonService_IsSingleton()
+  {
+    Assert.IsNotNull(SingletonService.Instance);
+    Assert.AreSame(SingletonService.Instance, SingletonService.Instance);
+  }
+}
