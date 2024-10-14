1c06a9e1d5700cd17c642bc36e00b3291e6e4e1f
Joni Laukka <jonilaukka@hotmail.com>
2024-10-14 20:53:04 +0300

    Linear Search

---
+++ Algorithms/C#/Algorithms/Algorithms/LinearSearch.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms;
+
+/// <summary>
+/// Linear search algorithm.
+/// </summary>
+/// Big O: O(n)
+public static class LinearSearch
+{
+  /// <summary>
+  /// Searches the index of the item in the array.
+  /// </summary>
+  /// <returns>Index of the item in the array. Returns -1 if the item is not in the array.</returns>
+  public static int IndexOf(int[] array, int item)
+  {
+    for (var i = 0; i < array.Length; i++)
+      if (array[i] == item) return i;
+
+    return -1;
+  }
+}
---
+++ Algorithms/C#/UnitTests/LinearSearchTests.cs
@@ -0,0 +1,13 @@
+﻿using Algorithms;
+
+namespace UnitTests;
+
+[TestClass]
+public class LinearSearchTests
+{
+  private static int[] Items => [1, 4, 3, 5, 6];
+
+  [TestMethod] public void Search_Found_ReturnIndex() => Assert.AreEqual(1, LinearSearch.IndexOf(Items, 4));
+
+  [TestMethod] public void Search_NotFound_ReturnDefault() => Assert.AreEqual(-1, LinearSearch.IndexOf(Items, 2));
+}
