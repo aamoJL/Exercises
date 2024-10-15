3bd11c8478b25bbf886f91c1b44b4c4d390aa96a
Joni Laukka <jonilaukka@hotmail.com>
2024-10-15 14:02:59 +0300

    Binary Search

---
+++ Algorithms/C#/Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿namespace Algorithms.Algorithms;
+
+/// <summary>
+/// Search algorithm for sorted array
+/// </summary>
+/// Big O: O(log(n))
+public static class BinarySearch
+{
+  /// <summary>
+  /// Searches the array for the given item.
+  /// </summary>
+  /// <returns><see langword="true"/> if the item exits in the array, otherwise <see langword="false"/></returns>
+  public static bool Exists(int[] array, int item)
+  {
+    var low = 0;
+    var high = array.Length - 1;
+    var index = high / 2;
+
+    while (low < high)
+    {
+      if (array[index] == item) break;
+      else if (array[index] > item) high = index - 1;
+      else if (array[index] < item) low = index + 1;
+
+      index = low + (high - low) / 2;
+    };
+
+    return array.Length > index && array[index] == item;
+  }
+
+  /// <summary>
+  /// Searches the array for the given item.
+  /// </summary>
+  /// <returns><see langword="true"/> if the item exits in the array, otherwise <see langword="false"/></returns>
+  public static bool ExistsAlternative(int[] array, int item)
+  {
+    // same as Exists, but without equality check inside the loop
+
+    var low = 0;
+    var high = array.Length - 1;
+    var index = high / 2;
+
+    while (low < high)
+    {
+      if (array[index] < item) low = index + 1;
+      else high = index;
+
+      index = low + (high - low) / 2;
+    };
+
+    return array.Length > index && array[index] == item;
+  }
+}
---
+++ Algorithms/C#/UnitTests/BinarySearchTests.cs
@@ -0,0 +1,87 @@
+﻿using Algorithms.Algorithms;
+
+namespace UnitTests;
+
+[TestClass]
+public class BinarySearchTests
+{
+  private static int[] CreateSet(int count, int start = 1)
+  {
+    var array = new int[count];
+
+    for (var i = 0; i < array.Length; i++)
+      array[i] = start + i;
+
+    return array;
+  }
+
+  [TestMethod]
+  public void Exists_Found_ReturnTrue()
+  {
+    var set = CreateSet(100, start: -20);
+
+    for (var i = 1; i < set.Length; i++)
+    {
+      var items = set.Take(i).ToArray();
+
+      foreach (var item in items)
+        Assert.IsTrue(BinarySearch.Exists(items, item), $"{item} was not found");
+    }
+  }
+
+  [TestMethod]
+  public void Exists_ZeroItems_NotFound_ReturnFalse()
+  {
+    var items = CreateSet(0);
+
+    foreach (var item in items)
+      Assert.IsFalse(BinarySearch.Exists(items, item), $"{item} was found");
+
+    Assert.IsFalse(BinarySearch.Exists(items, 1));
+  }
+
+  [TestMethod]
+  public void Exists_NotFound_ReturnFalse()
+  {
+    var set = CreateSet(10);
+
+    Assert.IsFalse(BinarySearch.Exists(set, 11));
+    Assert.IsFalse(BinarySearch.Exists(set, 0));
+    Assert.IsFalse(BinarySearch.Exists(set, -1));
+  }
+
+  [TestMethod]
+  public void ExistsAlternative_Found_ReturnTrue()
+  {
+    var set = CreateSet(100, start: -20);
+
+    for (var i = 1; i < set.Length; i++)
+    {
+      var items = set.Take(i).ToArray();
+
+      foreach (var item in items)
+        Assert.IsTrue(BinarySearch.ExistsAlternative(items, item), $"{item} was not found");
+    }
+  }
+
+  [TestMethod]
+  public void ExistsAlternative_ZeroItems_NotFound_ReturnFalse()
+  {
+    var items = CreateSet(0);
+
+    foreach (var item in items)
+      Assert.IsFalse(BinarySearch.ExistsAlternative(items, item), $"{item} was found");
+
+    Assert.IsFalse(BinarySearch.ExistsAlternative(items, 1));
+  }
+
+  [TestMethod]
+  public void ExistsAlternative_NotFound_ReturnFalse()
+  {
+    var set = CreateSet(10);
+
+    Assert.IsFalse(BinarySearch.ExistsAlternative(set, 11));
+    Assert.IsFalse(BinarySearch.ExistsAlternative(set, 0));
+    Assert.IsFalse(BinarySearch.ExistsAlternative(set, -1));
+  }
+}
