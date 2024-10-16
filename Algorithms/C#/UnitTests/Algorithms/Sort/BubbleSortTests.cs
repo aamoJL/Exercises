983b02ac3285bc7587f4b4fe9c29045b07c819c7
Joni Laukka <jonilaukka@hotmail.com>
2024-10-16 15:21:41 +0300

    Bubble Sort empty array unit tests

--- Algorithms/C#/UnitTests/Algorithms/Sort/BubbleSortTests.cs
+++ Algorithms/C#/UnitTests/Algorithms/Sort/BubbleSortTests.cs
@@ -16,6 +16,14 @@ public class BubbleSortTests
   }
 
   [TestMethod]
+  public void Sort_Empty_NoErrors()
+  {
+    var array = Array.Empty<int>();
+
+    BubbleSort.Sort(array);
+  }
+
+  [TestMethod]
   public void SortOptimized_Sorted()
   {
     var array = new int[] { 1, 3, 7, 4, 1, 2 };
@@ -23,5 +31,13 @@ public class BubbleSortTests
     BubbleSort.SortOptimized(array);
 
     CollectionAssert.AreEqual(new int[] { 1, 1, 2, 3, 4, 7 }, array);
+  }
+
+  [TestMethod]
+  public void SortOptimized_Empty_NoErrors()
+  {
+    var array = Array.Empty<int>();
+
+    BubbleSort.SortOptimized(array);
   }
 }
