e6bc139d938164f23985fb7a4f9e5646adffef23
Joni Laukka <jonilaukka@hotmail.com>
2024-10-21 15:34:44 +0300

    Solution renamed to "Algorithms", BasicGridPathFinder

---
+++ Algorithms/C#/Algorithms/Algorithms/PathFinding/BasicGridPathFinder.cs
@@ -0,0 +1,80 @@
+﻿namespace Algorithms.Algorithms.PathFinding;
+
+public static class BasicGridPathFinder
+{
+  public readonly struct Cell(int row, int col)
+  {
+    public int Row { get; } = row;
+    public int Col { get; } = col;
+  };
+
+  private static readonly (int row, int col)[] _directions = [
+      (-1, 0), // Up
+      (0, -1), // Left
+      (1, 0), // Down
+      (0, 1)]; // Right
+
+  public static bool FindPath<T>(T[][] grid, T startValue, T endValue, T[] invalidCellValues, out Cell[] steps)
+  {
+    var path = new DataStructures.Stack<Cell>();
+    var found = false;
+
+    if (FindCell(grid, startValue) is Cell startingCell && FindCell(grid, endValue) is not null)
+      found = WalkPath(grid, invalidCellValues, endValue, startingCell, path);
+
+    steps = path.ToArray(DataStructures.Stack<Cell>.ArrayOrder.LastIsFirst);
+
+    return found;
+  }
+
+  private static bool WalkPath<T>(T[][] grid, T[] invalidCellValues, T endValue, Cell current, DataStructures.Stack<Cell> path)
+  {
+    #region Base Case
+
+    // Path overlap
+    if (path.Contains(current))
+      return false;
+
+    // Out of Bounds
+    if (current.Row < 0 || current.Row >= grid.Length || current.Col < 0 || current.Col >= grid[0].Length)
+      return false;
+
+    var currentValue = grid[current.Row][current.Col];
+
+    // Reached the End
+    if (currentValue?.Equals(endValue) is true)
+    {
+      path.Push(current);
+      return true;
+    }
+
+    // Current cell is invalid (wall etc.)
+    if (invalidCellValues.Contains(currentValue))
+      return false;
+
+    path.Push(current); // Valid cell
+
+    #endregion
+
+    #region Recursive Case
+
+    foreach (var (rowDir, colDir) in _directions)
+      if (WalkPath(grid, invalidCellValues, endValue, current: new Cell(current.Row + rowDir, current.Col + colDir), path))
+        return true;
+
+    #endregion
+
+    path.Pop(); // Go back to the previous cell
+
+    return false; // No path found
+  }
+
+  private static Cell? FindCell<T>(T[][] grid, T value)
+  {
+    for (var row = 0; row < grid.Length; row++)
+      if (Array.IndexOf(grid[row], value) is var index && index != -1)
+        return new Cell(row, index);
+
+    return null;
+  }
+}
--- Algorithms/C#/Algorithms/Algorithms/PathFinding/Problems/BasicMazeSolver.cs
+++ Algorithms/C#/Algorithms/Algorithms/PathFinding/Problems/BasicMazeSolver.cs
@@ -2,72 +2,10 @@
 
 public static class BasicMazeSolver
 {
-  public static (int row, int index)[] Solve(char[][] maze)
+  public static (int row, int col)[] Solve(char[][] maze)
   {
-    var path = new DataStructures.Stack<(int row, int index)>();
+    BasicGridPathFinder.FindPath(maze, 'S', 'E', ['#'], out var steps);
 
-    // Find starting cell
-    var start = new Func<(int row, int index)?>(() =>
-    {
-      for (var row = 0; row < maze.Length; row++)
-        if (Array.IndexOf(maze[row], 'S') is var index && index != -1)
-          return (row, index);
-
-      return null;
-    })();
-
-    if (start != null)
-      FindPath(maze, start!.Value, path);
-
-    return path.ToArray(DataStructures.Stack<(int row, int index)>.ArrayOrder.LastIsFirst);
-  }
-
-  private static bool FindPath(char[][] maze, (int row, int index) current, DataStructures.Stack<(int row, int index)> path)
-  {
-    (int row, int index)[] directions = [
-      (-1, 0), // Up
-      (0, -1), // Left
-      (1, 0), // Down
-      (0, 1), // Right
-      ];
-
-    #region Base Case
-
-    // Path overlap
-    if (path.Contains(current))
-      return false;
-
-    // Out of Bounds
-    if (current.row < 0 || current.row >= maze.Length || current.index < 0 || current.index >= maze[0].Length)
-      return false;
-
-    var currentValue = maze[current.row][current.index];
-
-    // Reached the End
-    if (currentValue == 'E')
-    {
-      path.Push(current);
-      return true;
-    }
-
-    // Hit a Wall
-    if (currentValue == '#')
-      return false;
-
-    path.Push(current); // Valid cell to continue from
-
-    #endregion Base Case
-
-    #region Recursive Case
-
-    foreach (var (rowDir, indexDir) in directions)
-      if (FindPath(maze, current: (current.row + rowDir, current.index + indexDir), path)) // Recursion
-        return true;
-
-    #endregion Recursive Case
-
-    path.Pop(); // Go back to the previous cell
-
-    return false; // No path found
+    return steps.Select(x => (x.Row, x.Col)).ToArray();
   }
 }
