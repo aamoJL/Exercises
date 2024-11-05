09cf1a0eb4bb2e70bf0433158391791dc99cc861
Joni Laukka <jonilaukka@hotmail.com>
2024-11-05 20:52:10 +0200

    WeightedAdjacencyMatrixBreadthFirstSearch

---
+++ Algorithms/C#/Algorithms/Algorithms/Search/WeightedAdjacencyMatrixBreadthFirstSearch.cs
@@ -0,0 +1,77 @@
+﻿namespace Algorithms.Algorithms.Search;
+
+/*
+ * Weighted adjucency matrix example:
+ * 
+ * Graph:
+ * 0 → 1
+ * ↑ ↘ ↑
+ * 2 ← 3
+ * 
+ * Matrix:
+ *    0  1  2  3
+ * 0 [0, 1, 0, 1],
+ * 1 [0, 0, 0, 0],
+ * 2 [1, 0, 0, 0],
+ * 3 [0, 1, 1, 0],
+ */
+
+/// <summary>
+/// Breath-first search for weighted adjacency matrix
+/// </summary>
+public static class WeightedAdjacencyMatrixBreadthFirstSearch
+{
+  /// <summary>
+  /// Returns path between <paramref name="from"/> and <paramref name="to"/> nodes
+  /// </summary>
+  /// <returns>Array of node indexes or empty array if no path was found</returns>
+  /// <exception cref="ArgumentOutOfRangeException"></exception>
+  public static int[] GetPath(int[][] graph, int from, int to)
+  {
+    ArgumentOutOfRangeException.ThrowIfNegative(from);
+    ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(from, graph.Length);
+
+    if (to < 0 || to >= graph.Length)
+      return [];
+
+    var seen = new bool[graph.Length];
+    var prev = new int[graph.Length];
+    var queue = new DataStructures.Queue<int>([from]);
+
+    Array.Fill(prev, -1);
+    seen[from] = true;
+
+    while (queue.Count > 0)
+    {
+      var current = queue.Dequeue();
+
+      if (Equals(current, to))
+        break;
+
+      var connections = graph[current];
+
+      for (var i = 0; i < connections.Length; i++)
+      {
+        if (connections[i] == 0 || seen[i])
+          continue;
+
+        seen[i] = true;
+        prev[i] = current;
+        queue.Enqueue(i);
+      }
+    }
+
+    var curr = to;
+    var pathStack = new DataStructures.Stack<int>();
+
+    while (prev[curr] != -1)
+    {
+      pathStack.Push(curr);
+      curr = prev[curr];
+    }
+
+    return pathStack.Count == 0
+      ? []
+      : [from, .. pathStack.ToArray(DataStructures.Stack<int>.ArrayOrder.FirstIsFirst)];
+  }
+}
---
+++ Algorithms/C#/UnitTests/AlgorithmTests/SearchTests/WeightedAdjacencyMatrixBreadthFirstSearchTests.cs
@@ -0,0 +1,108 @@
+﻿using Algorithms.Algorithms.Search;
+
+namespace UnitTests.AlgorithmTests.SearchTests;
+
+[TestClass]
+public class WeightedAdjacencyMatrixBreadthFirstSearchTests
+{
+  [TestMethod]
+  public void GetPath_Exists_PathReturned()
+  {
+    /// 0 → 1
+    /// ↑ ↘ ↑
+    /// 2 ← 3
+    var graph = new int[][]
+    {
+      [0, 1, 0, 1],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 1, 1, 0],
+    };
+
+    // 0 -> 2
+    var expected = new int[] { 0, 3, 2 };
+    var actual = WeightedAdjacencyMatrixBreadthFirstSearch.GetPath(graph, 0, 2);
+
+    CollectionAssert.AreEqual(expected, actual);
+  }
+
+  [TestMethod]
+  public void GetPath_DoesNotExist_EmptyReturned()
+  {
+    /// 0 → 1
+    /// ↑ ↘ ↑
+    /// 2 ← 3
+    var graph = new int[][]
+    {
+      [0, 1, 0, 1],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 1, 1, 0],
+    };
+
+    // 0 -> 4
+    var expected = Array.Empty<int>();
+    var actual = WeightedAdjacencyMatrixBreadthFirstSearch.GetPath(graph, 0, 4);
+
+    CollectionAssert.AreEqual(expected, actual);
+  }
+
+  [TestMethod]
+  public void GetPath_Inaccessible_EmptyReturned()
+  {
+    /// 0 → 1
+    /// ↑ ↘ ↑
+    /// 2 ← 3
+    var graph = new int[][]
+    {
+      [0, 1, 0, 1],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 1, 1, 0],
+    };
+
+    // 1 -> 3
+    var expected = Array.Empty<int>();
+    var actual = WeightedAdjacencyMatrixBreadthFirstSearch.GetPath(graph, 1, 3);
+
+    CollectionAssert.AreEqual(expected, actual);
+  }
+
+  [TestMethod]
+  public void GetPath_OutOfRange_ExceptionThrown()
+  {
+    /// 0 → 1
+    /// ↑ ↘ ↑
+    /// 2 ← 3
+    var graph = new int[][]
+    {
+      [0, 1, 0, 1],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 1, 1, 0],
+    };
+
+    // 4 -> 0
+    Assert.ThrowsException<ArgumentOutOfRangeException>(
+      () => WeightedAdjacencyMatrixBreadthFirstSearch.GetPath(graph, graph.Length, 0));
+  }
+
+  [TestMethod]
+  public void GetPath_Negative_ExceptionThrown()
+  {
+    /// 0 → 1
+    /// ↑ ↘ ↑
+    /// 2 ← 3
+    var graph = new int[][]
+    {
+      [0, 1, 0, 1],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 1, 1, 0],
+    };
+
+    // 4 -> 0
+    Assert.ThrowsException<ArgumentOutOfRangeException>(
+      () => WeightedAdjacencyMatrixBreadthFirstSearch.GetPath(graph, -1, 0));
+  }
+}
