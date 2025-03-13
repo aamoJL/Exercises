ab688936763b74e6f1f9b9a2942f0b8a4d03102e
Joni Laukka <jonilaukka@outlook.com>
2025-03-13 21:03:55 +0200

    Improved theme styling

--- CSS/index.js
+++ CSS/index.js
@@ -1,18 +1,18 @@
 class CellContainer extends HTMLElement {
   constructor(props) {
     super(props);
-    
+
     const template = document.getElementById("cell-container-template");
-    const shadow = this.attachShadow({mode: "open"});
+    const shadow = this.attachShadow({ mode: "open" });
     const content = template.content.cloneNode(true);
-    
+
     shadow.appendChild(content);
   }
-  
+
   connectedCallback() {
     const shadow = this.shadowRoot;
-    const title = this.getAttribute("title");
-    
+    const title = this.getAttribute("cell-title");
+
     shadow.getElementById("title").innerHTML = title;
   }
 }
