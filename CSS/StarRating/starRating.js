5fbe5113cc8947a74ef5728d32a8b0f3ef7f7d14
Joni Laukka <jonilaukka@outlook.com>
2025-03-17 16:14:06 +0200

    Star Rating range selection

--- CSS/StarRating/starRating.js
+++ CSS/StarRating/starRating.js
@@ -1,31 +1,105 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const starContainer = document.getElementById("star-container");
-  const valueText = document.getElementById("rating-value");
+class StarRating {
+  valueRange = {
+    start: 0,
+    end: 0
+  };
+  visualRange = {
+    start: 0,
+    end: 0
+  };
+  dragging = false;
+  dragStart = 0;
+}
 
-  let currentValue = 0;
+const starRating = new StarRating();
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("value-setters-container").addEventListener("pointerleave", () => {
+    starRating.dragging = false;
+
+    updateVisual(starRating.valueRange);
+  })
 
   document.getElementsByName("rating-value-setter").forEach(element => {
     const value = element.getAttribute("data-value");
 
     element.addEventListener("mouseenter", () => {
-      if (starContainer) {
-        starContainer.style.width = `${(value / 5 * 100).toString()}%`;
-        starContainer.classList.add("hover");
+      if (starRating.dragging) {
+        starRating.visualRange.start = Math.min(starRating.dragStart, value);
+        starRating.visualRange.end = Math.max(starRating.dragStart, value);
       }
+      else
+        starRating.visualRange = { start: 0, end: value };
+
+      updateVisual(starRating.visualRange, true);
     });
 
-    element.addEventListener("mouseleave", () => {
-      if (starContainer) {
-        starContainer.style.width = `${(currentValue / 5 * 100).toString()}%`;
-        starContainer.classList.remove("hover");
-      }
+    element.addEventListener("dragstart", (e) => {
+      e.preventDefault();
+      starRating.dragging = true;
+      starRating.visualRange.start = value;
+      starRating.dragStart = value;
+
+      updateVisual(starRating.visualRange, true);
     });
 
-    element.addEventListener("click", (e) => {
-      currentValue = currentValue === value ? 0 : value;
+    element.addEventListener("pointerup", () => {
+      if (starRating.dragging) {
+        starRating.valueRange = {
+          start: starRating.visualRange.start,
+          end: starRating.visualRange.end
+        }
 
-      if (valueText)
-        valueText.innerText = currentValue;
+        updateValueVisual(starRating.valueRange);
+        updateVisual(starRating.valueRange);
+      }
+
+      starRating.dragging = false;
+    });
+
+    element.addEventListener("click", () => {
+      var newValue = starRating.valueRange.start === value && starRating.valueRange.end ? 0 : value;
+      starRating.valueRange = {
+        start: newValue,
+        end: newValue,
+      };
+
+      updateValueVisual(starRating.valueRange);
+      updateVisual(starRating.valueRange);
     })
   });
 });
+
+/**
+ * @param {{start: number, end: number}} range
+*/
+function updateValueVisual(range) {
+  const valueText = document.getElementById("rating-value");
+
+  if (valueText) {
+    if (range.start === range.end)
+      valueText.innerText = range.start;
+    else
+      valueText.innerText = `${range.start} - ${range.end}`;
+  }
+}
+
+/**
+ * @param {{start: number, end: number}} range
+ * @param {boolean} hover
+*/
+function updateVisual(range, hover = false) {
+  const visualContainer = document.getElementById("value-visual-container");
+
+  if (visualContainer) {
+    const visualStart = range.start === range.end ? 0 : range.start - 1;
+    const visualEnd = range.end;
+
+    visualContainer.style.clipPath = `inset(0% ${100 - (visualEnd / 5 * 100)}% 0% ${(visualStart / 5 * 100)}%)`;
+
+    if (hover)
+      visualContainer.classList.add("hover");
+    else
+      visualContainer.classList.remove("hover");
+  }
+}
